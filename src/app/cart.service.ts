903cd816df13a44c7e1e57a9024b1b551dad627b
oliverwreath <skywalkerhunter@gmail.com>
2019-06-02 17:46:27 -0400

    HttpClient get()

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
   items = [];
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
 
   addToCart(product) {
     this.items.push(product);
@@ -21,4 +25,7 @@ export class CartService {
     return this.items;
   }
 
+  getShippingPrices() {
+    return this.http.get('/assets/shipping.json');
+  }
 }
